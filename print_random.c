b717f3574e2327c491ea81a8e2d895c4b1427139
tun67213 <tun67213@temple.edu>
2024-01-17 21:00:46 -0500

    Update print_random.c and random.c
    Create the necessary functions in random.c for printing a random character.

--- print_random.c
+++ print_random.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
+#include "random.c"
 
 int main()
 {
---
+++ random.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char randchar()
+{
+    
+    return NULL;
+}
+
+int main(int argc, char** argv)
+{
+    
+    return 0;
+}
